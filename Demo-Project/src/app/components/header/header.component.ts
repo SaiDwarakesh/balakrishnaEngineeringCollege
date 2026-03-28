592e0bf6f45f0a5113997d738a248922bd7ae003
Sai Dwarakesh <dwarakesh@snovasys.com>
2026-03-28 16:56:22 +0530

    added new requirements

--- Demo-Project/src/app/components/header/header.component.ts
+++ Demo-Project/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
@@ -11,6 +11,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  isSticky = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isSticky = window.scrollY > 50;
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
